921b19c0151e9014eff6752e4d424017d4306284
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-15 20:38:18 +0100

    commit

--- ArrayvsArraylist/Program.cs
+++ ArrayvsArraylist/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,25 @@ namespace ArrayvsArraylist
     {
         static void Main(string[] args)
         {
+            const int miliarda = 1000000000;
+            int[] miliardaPole = new int[miliarda];
+            ArrayList miliardaArraylist = new ArrayList();
+
+            Random r = new Random();
+            Stopwatch stopky = new Stopwatch();
+
+            stopky.Start();
+            for (int i = 0; i < miliardaPole.Length; i++)
+            {
+                miliardaPole[i] = r.Next();
+            }
+
+
+            for (int i = 0; i < miliardaArraylist.Count; i++)
+            {
+                miliardaArraylist.Add(r.Next());
+            }
+
         }
     }
 }
